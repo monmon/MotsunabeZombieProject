17089bd747c8cb4db098c8485d26f3c5b2315076
monmon <lesamoureuses@gmail.com>
2013-12-12 23:49:17 +0900

    課題1で@や#が入った場合でも判定できるように対応

--- src/VOYAGE/Tweet.php
+++ src/VOYAGE/Tweet.php
@@ -4,8 +4,25 @@ namespace VOYAGE;
 
 class Tweet
 {
+    const DELIMITER = "\t";
+
+    protected $_screenName;
+    protected $_body;
+
+    public function __construct($str)
+    {
+        list($this->_screenName, $this->_body) = explode(self::DELIMITER, $str, 2);
+    }
+
     public function isNormal()
     {
+        if (preg_match('/@/', $this->_body)) {
+            return false;
+        }
+        if (preg_match('/#/', $this->_body)) {
+            return false;
+        }
+
         return true;
     }
 }
--- test/Tweet.php
+++ test/Tweet.php
@@ -6,12 +6,24 @@ use \VOYAGE\Tweet;
 
 class Tweetextends extends PHPUnit_Framework_TestCase
 {
-    public function test_Tweetを取り込み、オブジェクトとして保持する()
+    /**
+     * @dataProvider data_tweet
+     */
+    public function test_Tweetを取り込み、オブジェクトとして保持する($message, $expected, $str)
     {
-        $str = "monmon\tこれは普通のtweetです";
         $tweet = new Tweet($str);
 
         $this->assertTrue(method_exists($tweet, 'isNormal'), '普通のTweetと判定できるmethodを持っているか');
-        $this->assertSame(true, $tweet->isNormal(), '普通のTweetと判定できるか');
+        $this->assertSame($expected, $tweet->isNormal(), $message);
+    }
+
+    public function data_tweet()
+    {
+        return array(
+            array('普通のTweetと判定できるか', true, "monmon\tこれは普通のtweetです"),
+            array('普通のTweetとではないと判定できるか', false, "monmon\t@twitter これはreplyのtweetです"),
+            array('普通のTweetとではないと判定できるか', false, "monmon\tmention @twitter これはmentionのtweetです"),
+            array('普通のTweetとではないと判定できるか', false, "monmon\t#hashtab これはhashtagのtweetです"),
+        );
     }
 }
